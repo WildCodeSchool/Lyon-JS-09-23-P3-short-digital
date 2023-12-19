8a958aa3e05b57f1dddbbf86ae4fad2f4f561d92
Cedric-Font <font.cedric38@gmail.com>
2023-12-19 16:12:20 +0100

    modifi img balise by map on table and delete one line commit in navBar

--- frontend/src/layout/NavMobile/NavMobile.jsx
+++ frontend/src/layout/NavMobile/NavMobile.jsx
@@ -1,13 +1,44 @@
 import styles from "./NavMobile.module.css";
 
 export default function NavMobile() {
+  const icon = [
+    {
+      src: "src/assets/home.svg",
+      alt: "home logo",
+      id: 1,
+    },
+    {
+      src: "src/assets/search.svg",
+      alt: "search logo",
+      id: 2,
+    },
+    {
+      src: "src/assets/circle.svg",
+      alt: "add circle logo",
+      id: 3,
+    },
+    {
+      src: "src/assets/favorite.svg",
+      alt: "favorite logo",
+      id: 4,
+    },
+    {
+      src: "src/assets/profile.png",
+      alt: "profile logo",
+      id: 5,
+    },
+  ];
+
   return (
     <nav className={styles.navmobile}>
-      <img className={styles.icon} src="src/assets/home.png" alt="" />
-      <img className={styles.icon} src="src/assets/search.png" alt="" />
-      <img className={styles.icon} src="src/assets/add.png" alt="" />
-      <img className={styles.icon} src="src/assets/favoris.png" alt="" />
-      <img className={styles.icon} src="src/assets/profile.png" alt="" />
+      {icon.map((element) => (
+        <img
+          src={element.src}
+          key={element.id}
+          alt={element.alt}
+          className={styles.icon}
+        />
+      ))}
     </nav>
   );
 }
