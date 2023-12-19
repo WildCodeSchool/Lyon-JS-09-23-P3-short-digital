238495745b7c2b5de3b44dcbbeed53aeea07b84e
Cedric-Font <font.cedric38@gmail.com>
2023-12-19 18:24:05 +0100

    increase css navmobile

--- frontend/src/layout/NavMobile/NavMobile.jsx
+++ frontend/src/layout/NavMobile/NavMobile.jsx
@@ -6,38 +6,45 @@ export default function NavMobile() {
       src: "src/assets/home.svg",
       alt: "home logo",
       id: 1,
+      path: "#path for eslint",
     },
     {
       src: "src/assets/search.svg",
       alt: "search logo",
       id: 2,
+      path: "#path for eslint",
     },
     {
       src: "src/assets/circle.svg",
       alt: "add circle logo",
       id: 3,
+      path: "#path for eslint",
     },
     {
       src: "src/assets/favorite.svg",
       alt: "favorite logo",
       id: 4,
+      path: "#path for eslint",
     },
     {
-      src: "src/assets/profile.png",
+      src: "src/assets/account.svg",
       alt: "profile logo",
       id: 5,
+      path: "#path for eslint",
     },
   ];
 
   return (
     <nav className={styles.navmobile}>
       {icon.map((element) => (
-        <img
-          src={element.src}
-          key={element.id}
-          alt={element.alt}
-          className={styles.icon}
-        />
+        <a href={element.path} className={styles.aLink} key={element.id}>
+          <img
+            src={element.src}
+            alt={element.alt}
+            className={styles.icon}
+            id={element.id}
+          />
+        </a>
       ))}
     </nav>
   );
