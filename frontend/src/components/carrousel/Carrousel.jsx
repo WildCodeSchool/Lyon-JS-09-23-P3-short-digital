29d0eeb2a1337e1c836e3348d69354df00009204
Cedric-Font <font.cedric38@gmail.com>
2023-12-20 11:49:34 +0100

    work in progress

--- frontend/src/components/carrousel/Carrousel.jsx
+++ frontend/src/components/carrousel/Carrousel.jsx
@@ -1,7 +1,13 @@
 import { useEffect } from "react";
 
 export default function Carrouel() {
-    
-    useEffect{}
+  const [videosMiniature, setVideosMiniature] = useState("");
+  useEffect(() => {
+    (async () => {
+      const videoCall = await fetch("http://localhost:3310/api/videos/4");
+      const videoResult = await videoCall.json();
+            setVideosMiniature(videoResult);
+    })();
+    },[]);
     return
 }
