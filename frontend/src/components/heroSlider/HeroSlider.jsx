e204cfdc0616a10242bc6dc97f31c39a23d65af4
Cedric-Font <font.cedric38@gmail.com>
2023-12-20 09:30:23 +0100

    map for heroslider

--- frontend/src/components/heroSlider/HeroSlider.jsx
+++ frontend/src/components/heroSlider/HeroSlider.jsx
@@ -1,23 +1,49 @@
 import styles from "./HeroSlider.module.css";
 
 export default function HeroSlider() {
+  const img = [
+    {
+      id: "slide1",
+      src: "src/assets/CSS1.png",
+      alt: "css miniature videos",
+    },
+    {
+      id: "slide2",
+      src: "src/assets/CSS1.png",
+      alt: "css miniature videos",
+    },
+    {
+      id: "slide3",
+      src: "src/assets/CSS1.png",
+      alt: "css miniature videos",
+    },
+    {
+      id: "slide4",
+      src: "src/assets/CSS1.png",
+      alt: "css miniature videos",
+    },
+    {
+      id: "slide5",
+      src: "src/assets/CSS1.png",
+      alt: "css miniature videos",
+    },
+  ];
   return (
     <div className={styles.mainContainer}>
       <div className={styles.slider1}>
         <div className={styles.slider}>
-          <img id="slide1" src="src/assets/CSS1.png" alt="" />
-          <img id="slide2" src="src/assets/CSS2.png" alt="" />
-          <img id="slide3" src="src/assets/CSS3.png" alt="" />
-          <img id="slide4" src="src/assets/CSS4.png" alt="" />
-          <img src="src/assets/CSS1.png" alt="" />
+          {img.map((element) => (
+            <img id={element.id} src={element.src} alt={element.alt} />
+          ))}
         </div>
       </div>
       <div className={styles.sliderNav}>
-            <a href="#slide1"></a>
-            <a href="#slide2"></a>
-            <a href="#slide3"></a>
-            <a href="#slide4"></a>
-        </div>
+        <a href="#slide1">e</a>
+        {/* "e" are just here for eslint because if <a> are empty eslint saw an aerror */}
+        <a href="#slide2">e</a>
+        <a href="#slide3">e</a>
+        <a href="#slide4">e</a>
+      </div>
     </div>
   );
 }
