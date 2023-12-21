faa54e551a1552b7c660b32408841deb8c0dbf6c
celineblle <celineblle@outlook.fr>
2023-12-21 14:50:33 +0100

    refactorisation du css avec les modules et une nouvelle convention de nommage

--- frontend/src/components/boutonsLanguages/BoutonsLanguages.jsx
+++ frontend/src/components/boutonsLanguages/BoutonsLanguages.jsx
@@ -1,6 +1,6 @@
 // import { useEffect, useState } from "react";
 import { useState, useEffect } from "react";
-import "./boutonsLanguages.css";
+import styles from "./boutonsLanguages.module.css";
 
 function VideoPage() {
   const [actualCategory, setActualCategory] = useState("");
@@ -21,11 +21,11 @@ function VideoPage() {
   const categories = ["Javascript", "php", "Python", "Java", "css"];
 
   return (
-    <div id="buttons">
+    <div className={styles.buttonsContainer}>
       {categories.map((category) => (
         <button
-          id={category}
-          className="buttonCategory"
+          id={styles[category]}
+          className={styles.buttonsContainer__category}
           type="button"
           key={category}
           onClick={() => getCategory(category)}
