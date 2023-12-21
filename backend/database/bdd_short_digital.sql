7b0cfc839cbde13c2df23a5747625b8b35a43901
celineblle <celineblle@outlook.fr>
2023-12-21 15:54:59 +0100

    modification de la bdd pour avoir un cas de presentation plus reel

--- backend/database/bdd_short_digital.sql
+++ backend/database/bdd_short_digital.sql
@@ -63,7 +63,7 @@ CONSTRAINT beLiked
     REFERENCES video(id));
 
 INSERT INTO user (firstname, lastname, mail, pseudo) VALUES 
-('lulu', 'lapraline', 'luluentreprise@mail.com', 'Praluxor'), 
+('lulu', 'lapraline', 'luluentreprise@mail.com', 'TotoDu69'), 
 ('gary', 'tortue', 'tjrplusvite@studio.com', 'lievre'),
 ('aglae', 'martin', 'cupcake4life@mail.com', 'die4cakes'),
 ('ulrick', 'dupont', 'dout@dupont.com', 'Alexandre');
@@ -72,7 +72,7 @@ INSERT INTO video (title, link, image, description, weight, duration, user_id) V
 ('commencer le css', 'https://css-master.com/', 'cssM.png', 'commencer le css et devenez un expert en 2mn', '10mo', 120, 1),
 ('tout comprendre fonction asychrone js', 'https://jstoutcomprendre.com/', 'jsttc.jpg', 'vous avez rien compris mais vous voulez tout comprendre ?', '12mo', 122, 1),
 ('terrasser java', 'https://javaoupas.com/','java.png', 'javascript c est trop long a dire, mais java, cava ?', '13mo', 123, 2),
-('Johnny quenouille', 'https://firebasestorage.googleapis.com/v0/b/short-digital.appspot.com/o/quenouille.mp4?alt=media&token=98ef1dac-1a49-4eee-9b24-40466622f095', 'n', 'Ceci est une vidéo très instructive sur les quenouilles', '1kB', 91, 1);
+('Johnny quenouille', 'https://firebasestorage.googleapis.com/v0/b/short-digital.appspot.com/o/quenouille.mp4?alt=media&token=98ef1dac-1a49-4eee-9b24-40466622f095', 'n', 'Ceci est une vidéo très instructive sur les quenouilles. On prend le temps calmement de découvrir les quenouilles, et c est fantastique. Je suis fasciné par le fait que les quenouilles ont des grenouilles qui font des choses dans les quenouilles', '1kB', 91, 1);
 
 
 INSERT INTO category (name) VALUES ('css'), ('javaScript'), ('java'), ('Autre');
