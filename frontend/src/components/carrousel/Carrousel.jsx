9c24a304c69119a8edc7e381558c699214a89483
Cedric-Font <font.cedric38@gmail.com>
2024-01-05 10:24:01 +0100

    add call back carousel

--- frontend/src/components/carrousel/Carrousel.jsx
+++ frontend/src/components/carrousel/Carrousel.jsx
@@ -1,9 +1,28 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import { Swiper, SwiperSlide } from "swiper/react";
 import styles from "./Carrousel.module.css";
+import "swiper/css";
 
 export default function Carrousel({ title }) {
   const [videosMiniature, setVideosMiniature] = useState([]);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [screenHeight, setScreenHeight] = useState(window.innerHeight);
+
+  function size() {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+      setScreenHeight(window.innerHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }
+  size();
   useEffect(() => {
     (async () => {
       const videoCall = await fetch("http://localhost:3310/api/videos");
@@ -12,25 +31,40 @@ export default function Carrousel({ title }) {
     })();
   }, []);
   return (
-    <div className={styles.mainContainer}>
-      <p className={styles.title}>{title}</p>
-      <div className={styles.slider1}>
-        <div className={styles.slider}>
-          {videosMiniature.map((element) => (
-            <img
-              src={element.image}
-              alt=""
-              key={element.id}
+    <div className={styles.mainSwiper}>
+      <p>{title}</p>
+      <Swiper
+        slidesPerView={screenWidth >= 1100 ? 5 : 3}
+        spaceBetween={0}
+        className={styles.mainSwiper__swiper}
+      >
+        {videosMiniature.map((element) => (
+          <SwiperSlide
+            className={styles.mainSwiper__swiper__slide}
+            key={element.id}
+          >
+            <Link
               id={element.id}
-              className={styles.imgCarrousel}
-            />
-          ))}
-        </div>
-      </div>
+              className={styles.mainSwiper__swiper__slide__link}
+              to={{
+                pathname: `/video/${element.id}`,
+                search: element.title,
+              }}
+            >
+              <img
+                src={element.image}
+                alt=""
+                key={element.id}
+                id={element.id}
+                className={styles.mainSwiper__swiper__slide__link__imgs}
+              />
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 }
-
 Carrousel.propTypes = {
   title: PropTypes.string.isRequired,
 };
