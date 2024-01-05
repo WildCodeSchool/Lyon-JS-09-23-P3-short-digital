58f741eef59fbffc2d3d833617e8649a510d60a3
Cedric-Font <font.cedric38@gmail.com>
2024-01-05 12:38:54 +0100

    fix prettier

--- frontend/src/components/videos/Videos.jsx
+++ frontend/src/components/videos/Videos.jsx
@@ -1,38 +1,43 @@
-import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import styles from "./Videos.module.css";
 
 function Videos() {
-  const params = useParams();
-  const [videoInfo, setVideoInfo] = useState([]);
+  const [like, setLike] = useState(254);
 
-  useEffect(() => {
-    (async () => {
-      const videoCall = await fetch(
-        `http://localhost:3310/api/videos/${params.id}`
-      );
-      const videoResult = await videoCall.json();
-      setVideoInfo(videoResult);
-    })();
-  }, []);
+  const handleLike = () => {
+    setLike(like + 1);
+  };
 
   return (
     <div id={styles.videoContainer}>
-      <video id={styles.video} controls src={videoInfo.link}>
-        <track default kind="captions" src="../../ets/quenouilles.fr.vtt" />
+      <video
+        id={styles.video}
+        controls
+        src="https://firebasestorage.googleapis.com/v0/b/short-digital.appspot.com/o/quenouille.mp4?alt=media&token=98ef1dac-1a49-4eee-9b24-40466622f095%27,%20%27n%27,%20%27Ceci%20est%20une%20vid%C3%A9o%20tr%C3%A8s%20instructive%20sur%20les%20quenouilles"
+      >
+        <track default kind="captions" src="../../assets/quenouilles.fr.vtt" />
       </video>
       <div id={styles.informations}>
-        <h2 id={styles.informations__title}>{videoInfo.title}</h2>
+        <h2 id={styles.informations__title}>Les belles quenouilles</h2>
         <div id={styles.informations__owner}>
-          {/* <img src="./src/assets/profil.png" alt="" /> */}
+          <img src="./src/assets/profil.png" alt="" />
           <h3>Jeannette Doe</h3>
         </div>
         <div id={styles.informations__likes}>
-          <p>vues : 2542</p>
-          {/* <img alt="pouce en l'air" src="./src/assets/pouce.png" /> */}
-          <p>256</p>
+          <p>vues : 2569</p>
+          <button
+            type="button"
+            id={styles.informations__likes__button}
+            onClick={handleLike}
+          >
+            <img alt="pouce en l'air" src="./src/assets/pouce.png" />
+          </button>
+          <p>{like}</p>
         </div>
-        <p id={styles.informations__description}>{videoInfo.description}</p>
+        <p id={styles.informations__description}>
+          As-tu vu les belles quenouilles ? <br />
+          Ces belles quenouilles du Japon qui font face à l'afrique du sud ???
+        </p>
       </div>
     </div>
   );
