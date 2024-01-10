faac35f1f16572f1a6391bd70964a94197d6e135
Cedric-Font <font.cedric38@gmail.com>
2024-01-10 11:42:09 +0100

    work in progress

--- frontend/src/components/carrousel/Carrousel.jsx
+++ frontend/src/components/carrousel/Carrousel.jsx
@@ -1,29 +1,86 @@
-import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import useEmblaCarousel from "embla-carousel-react";
 import styles from "./Carrousel.module.css";
 
 export default function Carrousel({ title }) {
-  const [videosMiniature, setVideosMiniature] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const videoCall = await fetch("http://localhost:3310/api/videos");
-      const videoResult = await videoCall.json();
-      setVideosMiniature(videoResult);
-    })();
-  }, []);
+  const OPTIONS = {
+    slidesToScroll: "auto",
+    containScroll: "trimSnaps",
+    startIndex: 0,
+  };
+  const [emblaRef] = useEmblaCarousel(OPTIONS);
+  const img = [
+    {
+      id: "slide1",
+      src: "src/assets/CSS1.png",
+      alt: "css miniature videos",
+    },
+    {
+      id: "slide2",
+      src: "src/assets/CSS1.png",
+      alt: "css miniature videos",
+    },
+    {
+      id: "slide3",
+      src: "src/assets/CSS1.png",
+      alt: "css miniature videos",
+    },
+    {
+      id: "slide4",
+      src: "src/assets/CSS1.png",
+      alt: "css miniature videos",
+    },
+    {
+      id: "slide5",
+      src: "src/assets/CSS1.png",
+      alt: "css miniature videos",
+    },
+    {
+      id: "slide1",
+      src: "src/assets/CSS1.png",
+      alt: "css miniature videos",
+    },
+    {
+      id: "slide2",
+      src: "src/assets/CSS1.png",
+      alt: "css miniature videos",
+    },
+    {
+      id: "slide3",
+      src: "src/assets/CSS1.png",
+      alt: "css miniature videos",
+    },
+    {
+      id: "slide4",
+      src: "src/assets/CSS1.png",
+      alt: "css miniature videos",
+    },
+    {
+      id: "slide5",
+      src: "src/assets/CSS1.png",
+      alt: "css miniature videos",
+    },
+  ];
+
   return (
-    <div className={styles.mainContainer}>
-      <p className={styles.title}>{title}</p>
-      <div className={styles.slider1}>
-        <div className={styles.slider}>
-          {videosMiniature.map((element) => (
-            <img
-              src={element.image}
-              alt=""
+    <div className={styles.embla}>
+      <p>{title}</p>
+      <div className={styles.embla__viewport} ref={emblaRef}>
+        <div className={styles.embla__viewport__container}>
+          {img.map((element, index) => (
+            <div
+              className={styles.embla__viewport__container__slide}
               key={element.id}
-              id={element.id}
-              className={styles.imgCarrousel}
-            />
+            >
+              <div className={styles.embla__viewport__container__slide__number}>
+                <span>{index + 1}</span>
+              </div>
+              <img
+                className={styles.embla__viewport__container__slide__img}
+                src={element.src}
+                alt="Your alt text"
+              />
+            </div>
           ))}
         </div>
       </div>
