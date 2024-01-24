f249c719e05919d8a8c917d3460a2e3e05712a60
celineblle <celineblle@outlook.fr>
2024-01-24 10:16:29 +0100

    changement de regex

--- frontend/src/pages/inscription/DonneesFormulaire.jsx
+++ frontend/src/pages/inscription/DonneesFormulaire.jsx
@@ -19,8 +19,7 @@ function DonneesFormulaire() {
   const MIN_LENGTH_PASSWORD = 8;
   const MAX_LENGTH_PASSWORD = 200;
   const regexEmail =
-    // eslint-disable-next-line no-useless-escape
-    /^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([_\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,3})/;
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const regexPassword =
     /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+!*$@%_])([-+!*$@%_\w]{8,20})$/g;
 
